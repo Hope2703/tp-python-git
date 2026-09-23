9aa75a1f38ba27f694b768dcc306780d128e9715
Hope2703 <espoirmulela67@gmail.com>
2026-09-23 14:56:41 +0100

    refactor: separer la saisie et la conversion du nombre

--- ex2_parite.py
+++ ex2_parite.py
@@ -1,4 +1,5 @@
-nombre = int(input("Entrez un nombre entier : "))
+nombre_input = input("Entrez un nombre entier : ")
+nombre = int(nombre_input)
 
 if nombre % 2 == 0:
     print(f"Le nombre {nombre} est pair.")
